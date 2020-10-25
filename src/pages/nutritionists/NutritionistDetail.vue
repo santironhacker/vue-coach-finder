edd3d6e89c586680a04269e4f617b966fde48e1e
santironhacker <santi92.network@gmail.com>
2020-10-25 16:34:20 +0700

    Created content for NutritionistDetail page and passed id property to get the appropiate coach from the store.

--- src/pages/nutritionists/NutritionistDetail.vue
+++ src/pages/nutritionists/NutritionistDetail.vue
@@ -1,12 +1,59 @@
 <template>
   <p>Nutritionist Detail</p>
-  <router-view></router-view>
-  <router-link to="/nutritionists/n1/contact"></router-link>
+  <section>
+    <base-card>
+      <h2>{{ fullName }}</h2>
+      <h3>${{ rate }}/hour</h3>
+    </base-card>
+  </section>
+  <section>
+    <base-card>
+      <header>
+        <h2>Interested? Reach out now!</h2>
+        <base-button link :to="contactLink">Contact</base-button>
+      </header>
+      <router-view></router-view>
+    </base-card>
+  </section>
+  <section>
+    <base-card>
+      <base-badge v-for="area in areas" :key="area" :type="area" :title="area"></base-badge>
+      <p>{{ description }}</p>
+    </base-card>
+  </section>
 </template>
 
 <script>
 export default {
-
+  props: ['id'],
+  data() {
+    return {
+      selectedNutritionist: null
+    };
+  },
+  computed: {
+    fullName() {
+      return `${this.selectedNutritionist.firstName} ${this.selectedNutritionist.lastName}`;
+    },
+    contactLink() {
+      return `${this.$route.path}/contact`; // nutritionists/n1/contact
+    },
+    // Here we need to define areas, rate and description because the prop we receive is just an id.
+    // We are in charge to then point to the correct nutritionist field info desired.
+    areas() {
+        return this.selectedNutritionist.areas;
+      },
+    rate() {
+      return this.selectedNutritionist.hourlyRate;
+    },
+    description() {
+      return this.selectedNutritionist.description;
+    }
+  },
+  created() {
+    this.selectedNutritionist = this.$store.getters['nutritionists/nutritionists']
+    .find(nutritionist => nutritionist.id === this.id);
+  }
 }
 </script>
 
--- src/router.js
+++ src/router.js
@@ -3,7 +3,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 import NutritionistDetail from './pages/nutritionists/NutritionistDetail.vue';
 import NutritionistsList from './pages/nutritionists/NutritionistsList.vue';
 import NutritionistRegistration from './pages/nutritionists/NutritionistRegistration.vue';
-import NutritionistsCoach from './pages/requests/ContactNutritionist.vue';
+import ContactNutritionist from './pages/requests/ContactNutritionist.vue';
 import RequestsReceived from './pages/requests/RequestsReceived.vue';
 import NotFound from './pages/NotFound.vue';
 
@@ -17,9 +17,12 @@ const router = createRouter({
             path: '/nutritionists', component: NutritionistsList
         },
         {
-            path: '/nutritionists/:id', component: NutritionistDetail, children: [
+            path: '/nutritionists/:id', component: NutritionistDetail, 
+            // Enforces to receive :id as a prop in the component the route points to
+            props: true,
+            children: [
                 {
-                    path: 'contact', component: NutritionistsCoach
+                    path: 'contact', component: ContactNutritionist
                 }
             ]
         },
