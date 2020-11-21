8b27aab80edf4cbc815e06d79c3464b9b0717e6e
santironhacker <santi92.network@gmail.com>
2020-11-21 18:30:33 +0700

    Created getter methods for requests. Wired getters into index.js. Created a requests folder with RequestItems to loop through and output email and message. Outputting incoming requests in the RequestsReceived view.

--- src/pages/requests/RequestsReceived.vue
+++ src/pages/requests/RequestsReceived.vue
@@ -1,13 +1,54 @@
 <template>
-  <p>Requests Received</p>
+  <section>
+    <base-card>
+      <header>
+        <h2>Requests received</h2>
+      </header>
+      <ul v-if="hasRequests">
+        <request-item
+          v-for="req in receivedRequests"
+          :key="req.id"
+          :email="req.userEmail"
+          :message="req.message"
+        >
+        </request-item>
+      </ul>
+      <h3 v-else>You haven't received any requests yet!</h3>
+    </base-card>
+  </section>
 </template>
 
 <script>
-export default {
+import RequestItem from '../../components/requests/RequestItem.vue';
 
+export default {
+  components: {
+    RequestItem
+  },
+  computed: {
+    receivedRequests() {
+      return this.$store.getters['requests/requests'];
+    },
+    hasRequests() {
+      return this.$store.getters['requests/hasRequests'];
+    }
+  }
 }
 </script>
 
-<style>
+<style scoped>
+header {
+  text-align: center;
+}
 
+ul {
+  list-style: none;
+  margin: 2rem auto;
+  padding: 0;
+  max-width: 30rem;
+}
+
+h3 {
+  text-align: center;
+}
 </style>
