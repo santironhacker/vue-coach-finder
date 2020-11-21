276eb70a271e13e61ca997ae7bd1ecf509080ca9
santironhacker <santi92.network@gmail.com>
2020-11-21 18:52:34 +0700

    Updated requests/getters logic to return only data corresponding to the active user.

--- src/store/modules/requests/getters.js
+++ src/store/modules/requests/getters.js
@@ -1,8 +1,11 @@
 export default {
-    requests(state) {
-        return state.requests;
+    // Remember the convention to underscore unnused arguments
+    // ORIGINAL: state, getters, rootState, rootGetters
+    requests(state, _, _2, rootGetters) {
+        const nutritionistId = rootGetters.userId;
+        return state.requests.filter(req => req.nutritionistId === nutritionistId);
     },
-    hasRequests(state) {
-        return state.requests && state.requests.length > 0;
+    hasRequests(_, getters) {
+        return getters.requests && getters.requests.length > 0;
     }
 };
