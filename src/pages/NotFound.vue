866858f160b26adf37b58cbb1a425493982614d7
santironhacker <santi92.network@gmail.com>
2020-11-22 18:11:10 +0700

    Adding content to notFound page.

---
+++ src/pages/NotFound.vue
@@ -0,0 +1,20 @@
+<template>
+  <section>
+      <base-card>
+        <h2>Page not found</h2>
+        <p>This page could not be found. Maybe check all our 
+            <router-link to="nutritionists">nutritionists</router-link>.
+        </p>
+      </base-card>
+  </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
